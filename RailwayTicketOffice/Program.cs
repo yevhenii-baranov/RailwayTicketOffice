58a266e2e82475f09804441d328a14745f632c7f
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2018-12-22 19:43:44 +0200

    Move project SDK to .Net Core 3.0 and remove all files associated with ASP.NET

--- RailwayTicketOffice/Program.cs
+++ RailwayTicketOffice/Program.cs
@@ -1,17 +1,10 @@
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-
 namespace RailwayTicketOffice
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            
         }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
     }
 }
