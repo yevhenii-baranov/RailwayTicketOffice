08961ccb028271871fd1b285e666ecadbda36063
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2018-12-22 20:38:30 +0200

    Add stub for the main menu form.

--- RailwayTicketOffice/Program.cs
+++ RailwayTicketOffice/Program.cs
@@ -1,10 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
 namespace RailwayTicketOffice
 {
-    public class Program
+    static class Program
     {
-        public static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainMenu());
         }
     }
 }
