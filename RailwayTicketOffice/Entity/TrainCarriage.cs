67326834aa8a43aa1b39d53bcc3fb0166687cdbd
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2018-12-25 10:34:42 +0200

    Fix duplicate table name for the carriage in train.

--- RailwayTicketOffice/Entity/TrainCarriage.cs
+++ RailwayTicketOffice/Entity/TrainCarriage.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RailwayTicketOffice.Entity
 {
+    [Table("trainCarriage")]
     public class TrainCarriage
     {
         [Required] public Train Train { get; set; }
