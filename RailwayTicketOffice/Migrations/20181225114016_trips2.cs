eb0269af7d6524d52c6c39d80b0b93385bf10fd1
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2018-12-25 13:56:35 +0200

    Add new migration versions.

---
+++ RailwayTicketOffice/Migrations/20181225114016_trips2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RailwayTicketOffice.Migrations
+{
+    public partial class trips2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "trip",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_trip", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "trip");
+        }
+    }
+}
--- RailwayTicketOffice/Migrations/20181225114619_trips3.Designer.cs
+++ RailwayTicketOffice/Migrations/20181225114619_trips3.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RailwayTicketOffice.Database;
 
 namespace RailwayTicketOffice.Migrations
 {
     [DbContext(typeof(MySqlDbContext))]
-    partial class MySqlDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181225114619_trips3")]
+    partial class trips3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
