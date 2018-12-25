f340b4a8e523d90131f62b324e29f064f01dcbae
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2018-12-25 21:34:31 +0200

    Remove redundant code.

--- RailwayTicketOffice/Application.cs
+++ RailwayTicketOffice/Application.cs
@@ -14,22 +14,23 @@ namespace RailwayTicketOffice
         private static TicketOfficeApplication application = null;
 
         private readonly AuthenticationService authService = new AuthenticationService();
+        private readonly TrainFindingService trainFindingService = new TrainFindingService();
+
+
         public User CurrentUser { get; set; }
 
         private TicketOfficeApplication()
         {
-            using(var context = new MySqlDbContext())
-            {
-                if (!context.Database.EnsureCreated())
-                {
-                    context.Database.Migrate();
-                }
-            }
         }
 
         public AuthenticationService GetAuthenticationService()
         {
             return authService;
+        }
+
+        public TrainFindingService GetTrainFindingService()
+        {
+            return trainFindingService;
         }
 
         [MethodImpl(MethodImplOptions.Synchronized)]
