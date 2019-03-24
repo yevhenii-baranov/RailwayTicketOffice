6d88a16c2b2d5bc471d369bc764e14e2f5ae40bf
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2019-03-24 23:54:29 +0200

    Add more fields to user class.

--- RailwayTicketOffice/Entity/User.cs
+++ RailwayTicketOffice/Entity/User.cs
@@ -6,16 +6,22 @@ namespace RailwayTicketOffice.Entity
     [Table("user")]
     public class User : Entity
     {
-        [Required] public string FirstName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
 
-        [Required] public string LastName { get; set; }
+        [Required]
+        public string LastName { get; set; }
 
         public string PassportData { get; set; }
 
+        [DataType(DataType.EmailAddress)] 
+        public string Email { get; set; }
+
         public string Username { get; set; }
 
+        [DataType(DataType.Password)]
         public string Password { get; set; }
-
+        
         public Role UserRole { get; set; }
 
         public enum Role: int
