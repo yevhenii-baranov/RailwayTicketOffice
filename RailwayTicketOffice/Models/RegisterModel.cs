8d0ca9643a9b5caeee6f4343c4b81ada3b5659cf
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2019-03-25 23:04:14 +0200

    Add display names for registering model fields.

--- RailwayTicketOffice/Models/RegisterModel.cs
+++ RailwayTicketOffice/Models/RegisterModel.cs
@@ -1,17 +1,21 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace RailwayTicketOffice.Models
 {
     public class UserRegisterModel
     {
-        [Required(ErrorMessage = "Please enter valid Email")]
+        [DisplayName("Your Email")]
+        [Required(ErrorMessage = "Please enter valid Email:")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         
-        [Required(ErrorMessage = "Please enter password")]
+        [DisplayName("Your password")]
+        [Required(ErrorMessage = "Please enter password:")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
         
+        [DisplayName("Please enter password again:")]
         [Required(ErrorMessage = "Please enter password once again")]
         [DataType(DataType.Password)]
         public string PasswordConfirmation { get; set; }
