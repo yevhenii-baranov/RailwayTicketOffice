de1298dd962bc2f6e1fa364f31c637324b59a983
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2019-03-25 23:04:45 +0200

    Implement dashboard.

--- RailwayTicketOffice/Controllers/DashboardController.cs
+++ RailwayTicketOffice/Controllers/DashboardController.cs
@@ -1,14 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using RailwayTicketOffice.Entity;
+using RailwayTicketOffice.Models;
+using RailwayTicketOffice.Service;
 
 namespace RailwayTicketOffice.Controllers
 {
     [Authorize]
     public class DashboardController : Controller
     {
+        private readonly TrainFindingService _service = new TrainFindingService();
+        
         public IActionResult Index()
         {
-            return View();
+            var trips = _service.FindTrips(null, null, null);
+            var tripModels = trips.Select(trip => new TripModel
+            {
+                ID = trip.ID,
+                TripDate = trip.TripDate,
+                ArrivalStation = trip.Train.ArrivalStation.Name,
+                DepartureStation = trip.Train.DepartureStation.Name,
+                ArrivalTime = trip.Train.ArrivalTime,
+                DepartureTime = trip.Train.DepartureTime
+            });
+            return View(tripModels);
         }
     }
 }
---
+++ RailwayTicketOffice/Controllers/TicketBuyingController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RailwayTicketOffice.Controllers
+{
+    public class TicketBuyingController : Controller
+    {
+        public IActionResult Buy(string @class)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
