8da8e7682349317d3d97f846309783fcae8c0b32
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2019-03-26 22:44:25 +0200

    Add migrations that were not previously added.

---
+++ RailwayTicketOffice/Migrations/20190325170049_user-nullables-remove.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RailwayTicketOffice.Migrations
+{
+    public partial class usernullablesremove : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "user");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "user",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "user",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "user",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "user",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "user",
+                nullable: true);
+        }
+    }
+}
