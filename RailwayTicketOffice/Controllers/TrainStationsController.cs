ce7a145aa476ce7d67ea24ccd0fc3ad76100163f
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2019-03-30 14:01:03 +0200

    Add generated controllers for CRUD operations.

---
+++ RailwayTicketOffice/Controllers/TrainStationsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RailwayTicketOffice.Database;
+using RailwayTicketOffice.Entity;
+
+namespace RailwayTicketOffice
+{
+    public class TrainStationsController : Controller
+    {
+        private readonly MySqlDbContext _context;
+
+        public TrainStationsController(MySqlDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TrainStations
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Stations.ToListAsync());
+        }
+
+        // GET: TrainStations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainStation = await _context.Stations
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (trainStation == null)
+            {
+                return NotFound();
+            }
+
+            return View(trainStation);
+        }
+
+        // GET: TrainStations/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TrainStations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,ID")] TrainStation trainStation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(trainStation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(trainStation);
+        }
+
+        // GET: TrainStations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainStation = await _context.Stations.FindAsync(id);
+            if (trainStation == null)
+            {
+                return NotFound();
+            }
+            return View(trainStation);
+        }
+
+        // POST: TrainStations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,ID")] TrainStation trainStation)
+        {
+            if (id != trainStation.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(trainStation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TrainStationExists(trainStation.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(trainStation);
+        }
+
+        // GET: TrainStations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trainStation = await _context.Stations
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (trainStation == null)
+            {
+                return NotFound();
+            }
+
+            return View(trainStation);
+        }
+
+        // POST: TrainStations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var trainStation = await _context.Stations.FindAsync(id);
+            _context.Stations.Remove(trainStation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TrainStationExists(int id)
+        {
+            return _context.Stations.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ RailwayTicketOffice/Controllers/TripsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RailwayTicketOffice.Database;
+using RailwayTicketOffice.Entity;
+
+namespace RailwayTicketOffice
+{
+    public class TripsController : Controller
+    {
+        private readonly MySqlDbContext _context;
+
+        public TripsController(MySqlDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Trips
+        public async Task<IActionResult> Index()
+        {
+            var mySqlDbContext = _context.Trips.Include(t => t.Train);
+            return View(await mySqlDbContext.ToListAsync());
+        }
+
+        // GET: Trips/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trip = await _context.Trips
+                .Include(t => t.Train)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return View(trip);
+        }
+
+        // GET: Trips/Create
+        public IActionResult Create()
+        {
+            ViewData["TrainId"] = new SelectList(_context.Trains, "ID", "ID");
+            return View();
+        }
+
+        // POST: Trips/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TrainId,TripDate,ID")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(trip);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TrainId"] = new SelectList(_context.Trains, "ID", "ID", trip.TrainId);
+            return View(trip);
+        }
+
+        // GET: Trips/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trip = await _context.Trips.FindAsync(id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+            ViewData["TrainId"] = new SelectList(_context.Trains, "ID", "ID", trip.TrainId);
+            return View(trip);
+        }
+
+        // POST: Trips/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TrainId,TripDate,ID")] Trip trip)
+        {
+            if (id != trip.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(trip);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TripExists(trip.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TrainId"] = new SelectList(_context.Trains, "ID", "ID", trip.TrainId);
+            return View(trip);
+        }
+
+        // GET: Trips/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var trip = await _context.Trips
+                .Include(t => t.Train)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return View(trip);
+        }
+
+        // POST: Trips/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var trip = await _context.Trips.FindAsync(id);
+            _context.Trips.Remove(trip);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TripExists(int id)
+        {
+            return _context.Trips.Any(e => e.ID == id);
+        }
+    }
+}
