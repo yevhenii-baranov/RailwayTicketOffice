e4e3c6fd37176ab47e1bd7ce154be561a68b4097
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2019-04-01 09:59:39 +0300

    Update migrations.

---
+++ RailwayTicketOffice/Migrations/20190401065651_add-purchase-date.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RailwayTicketOffice.Migrations
+{
+    public partial class addpurchasedate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "ticket",
+                newName: "TripDate");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PurchaseDate",
+                table: "ticket",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PurchaseDate",
+                table: "ticket");
+
+            migrationBuilder.RenameColumn(
+                name: "TripDate",
+                table: "ticket",
+                newName: "Date");
+        }
+    }
+}
