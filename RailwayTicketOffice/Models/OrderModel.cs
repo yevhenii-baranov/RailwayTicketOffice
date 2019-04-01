d208b0c01bf7301015a092701f8483b3737d75f9
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2019-04-01 12:16:52 +0300

    Add order model.

--- RailwayTicketOffice/Models/OrderModel.cs
+++ RailwayTicketOffice/Models/OrderModel.cs
@@ -1,6 +1,28 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace RailwayTicketOffice.Models
 {
     public class OrderModel
     {
+        [DisplayName("Order identifier")]
+        public int TicketId { get; set; }
+        
+        [DisplayName("From")]
+        public string DepartureStation { get; set; }
+
+        [DisplayName("To")]
+        public string ArrivalStation { get; set; }
+
+        public decimal Price { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]        
+        [DisplayName("Date of the trip")]
+        public DateTime TripDate { get; set; }
+        
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]        
+        [DisplayName("Date of the purchase")]
+        public DateTime PurchaseDate { get; set; }
     }
 }
